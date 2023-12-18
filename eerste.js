f60a34d5d8c0beb3453d521e0fcbf7794aa88ba7
Vikkeyeftimov <Vikksaus@hotmail.com>
2023-12-18 13:35:53 +0100

    laatste werkje

--- eerste.js
+++ eerste.js
@@ -6,11 +6,10 @@ let sunny = document.querySelector(".sunny");
 button.addEventListener('click', veranderen)
 function veranderen(){
     sunny.classList.toggle("mooiweer")
-    slide.classList.toggle("mooiweer")
+    slide.classList.toggle("mooiweer") 
+    if(this.innerHTML === 'Zet zonnetje aan!') {
+        this.innerHTML = 'Laat het regenen!'
+    }  else {
+        this.innerHTML = 'Zet zonnetje aan!' 
+    }
 }
-
-// console.log(slide);
-
-// button.addEventListener("cl", function(){
-//     Element.classlist.toggle("sunny")
-// })
